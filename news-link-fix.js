b6df616c16bbdcc41f509c5d71a9ee51a9ee8390
yobukodori <32874862+yobukodori@users.noreply.github.com>
2019-09-12 21:23:22 +0900

    v.0.1.0

---
+++ news-link-fix.js
@@ -0,0 +1,157 @@
+/*
+ * title: news link fix v.0.1.0
+ * name: news-link-fix.js
+ * author: yobukodori
+*/
+
+(function(){
+	'use strict';
+	if (window.ybkdrNewsLinkFix)
+		return;
+	window.ybkdrNewsLinkFix = true;
+
+	function mutationsChildListForEach(mutations, callback, opt)
+	{
+		if (opt == null)
+			opt = {};
+		for (let i = 0 ; i < mutations.length ; i++){
+			let m = mutations[i];
+			if (m.type === "childList"){
+				if (opt.addedNodes == null || opt.addedNodes){
+					for (let j = 0 ; j < m.addedNodes.length ; j++)
+						callback(m.addedNodes[j],"added");
+				}
+				if (opt.removedNodes == null || opt.removedNodes){
+					for (let j = 0 ; j < m.removedNodes.length ; j++)
+						callback(m.removedNodes[j],"removed");
+				}
+			}
+		}
+	}
+	
+	function newtab(e)
+	{
+		e.setAttribute("target", "_blank");
+	}
+	
+	function underline(e)
+	{
+		e.setAttribute("style", "text-decoration: underline;");
+		let e2 = e.querySelector('dd') || e.querySelector('p');
+		if (e2)
+			e2.setAttribute("style", "text-decoration: underline;");
+	}
+
+	let siteData = {
+		"www.jiji.com": {
+			isTarget: function(e){
+				return true; // /\/sp\/(article|v\d|d\d|c|tokushu)\?/.test(e.href);
+			},
+			fixLink: function(e){
+				'use strict';
+				if (/\/sp\/(article|v\d|d\d|c|tokushu)\?/.test(e.href)){
+					e.setAttribute("onclick", "");
+				}
+			}
+		},
+		"mainichi.jp": {
+			isTarget: function(e){
+				return true; // /\/articles\/\d{8}\//.test(e.href);
+			}
+		},
+		"www.yomiuri.co.jp": {
+			isTarget: function(e){
+				return true; // /\/\d{8}-/.test(e.href);
+			}
+		},
+		"www.47news.jp": {
+			isTarget: function(e){
+				return true; // /\/\d+\.html/.test(e.href);
+			},
+			fixLink: function(e){
+				'use strict';
+				let attr = e.getAttribute("style");
+				attr = (attr ? attr + "; " : "") + "touch-action: initial";
+				e.setAttribute("style", attr);
+				if (/\/\d+\.html/.test(e.href)){
+					fetch(e.href)
+					.then(function(response) {
+						return response.text();
+					})
+					.then(function(html) {
+						let sig = '<a class="read-more-button"', i, r;
+						(i = html.indexOf(sig)) !== -1 && (r = html.substring(i, html.indexOf(">", i + sig.length) + 1).match('href="(.+?)"')) && (e.href = r[1]);
+					});			
+				}
+			}
+		},
+		"news.goo.ne.jp": {
+			isTarget: function(e){
+				return true; // /\/\d+\.html/.test(e.href);
+			},
+			fixLink: function(e){
+				'use strict';
+				if (/\/(article|topstories)\/.+\.html/.test(e.href)){
+					fetch(e.href)
+					.then(function(response) {
+						return response.text();
+					})
+					.then(function(html) {
+						let r, i = html.indexOf('id="topics_1_more"');
+						i != -1 && (r = html.substring(html.lastIndexOf('<', i), i).match('href="(.+?)"')) && (e.href = r[1]);
+					});			
+				}
+			}
+		},
+		"news.yahoo.co.jp": {
+			isTarget: function(e){
+				return true; // /\/\d+\.html/.test(e.href);
+			},
+			fixLink: function(e){
+				'use strict';
+				if (/\/(pickup\/\d+|article\?)/.test(e.href)){
+					fetch(e.href, {mode:"no-cors"})
+					.then(function(response) {
+						return response.text();
+					})
+					.then(function(html) {
+						let sig = /\/pickup\/\d+/.test(e.href) ? 'id="dtlBtn">' : 'class="tpcNews_detailLink">', i = html.indexOf(sig), r;
+						i != -1 && (r = html.substring(i + sig.length, html.indexOf('>', i + sig.length)).match('href="(.+?)"')) && (e.href = r[1]);
+					});			
+				}
+			},
+			postprocess: function(){
+				let fixLink = this.fixLink, ul = d.querySelector('ul.newsFeed_list');
+				if (ul){
+					(new MutationObserver(function(mutations, observer){
+						mutations.forEach(m=>{
+							if (m.type === "childList"){
+								for (let i = 0 ; i < m.addedNodes.length ; i++){
+									let e = m.addedNodes[i];
+									e.querySelector && (e = e.querySelector('a')) && (newtab(e), underline(e), fixLink(e));
+								}
+							}
+						});
+					})).observe(ul, {childList:true});
+				}
+				else {
+					console.log("ul not found");
+				}
+			}
+		},
+	};
+	let d = document;
+	let sd = siteData[d.location.hostname.toLowerCase()];
+	let ee = d.getElementsByTagName("a");
+	for (let i = 0 ; i < ee.length ; i++){
+		let e = ee[i];
+		if (! sd || ! sd.isTarget || sd.isTarget(e)){
+			newtab(e);
+			underline(e);
+		}
+		if (sd && sd.fixLink)
+			sd.fixLink(e);
+	}
+	if (sd && sd.postprocess)
+		sd.postprocess();
+})()
